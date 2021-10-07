4f85777e9081266c923f2cc0baede55addac4ed5
Fran Guijarro <franleplant@gmail.com>
2021-10-07 14:18:05 -0300

    Metamask integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Web3ReactProvider } from "@web3-react/core";
+import getLibrary from "../modules/wallet/getLibrary";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  //useEff
+
+  //if (!!window.ethereum) {
+  //window.ethereum.autoRefreshOnNetworkChange = false;
+  //}
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />;
+    </Web3ReactProvider>
+  );
 }
 export default MyApp;
