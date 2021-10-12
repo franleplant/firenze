8c5388d840cbd2e77825eae05fb559231a5b9932
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-10-12 11:59:57 -0300

    Changed tweet divison color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
 			<main className='relative flex flex-col sm:ml-10 border-gray-800 sm:w-3/5 sm:border-l sm:border-r'>
 				<Header />
 				<NewTweet />
-				<section className='px-4 sm:px-0 divide-y divide-yellow-800'>
+				<section className='px-4 sm:px-0 divide-y divide-blueGray-500'>
 					{tweets.map(({ author, content, date, id }) => (
 						<div key={id} className='p-2'>
 							<Tweet author={author} content={content} date={date} />
