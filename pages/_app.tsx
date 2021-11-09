f088426a12930083fdfcd8643f899fff81f407dc
Fran Guijarro <franleplant@gmail.com>
2021-11-08 22:45:51 -0300

    fixe race cond

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { IPFSProvider } from "components/IPFS";
-import { SelfIDProvider } from "components/SelfID";
-import { WalletProvider } from "components/Wallet";
+import { IPFSProvider, useIpfs } from "components/IPFS";
+import { SelfIDProvider, useSelfID } from "components/SelfID";
+import { useWallet, WalletProvider } from "components/Wallet";
 import { FirebaseProvider } from "components/Firebase";
 
 const queryClient = new QueryClient();
@@ -15,13 +15,27 @@ function MyApp({ Component, pageProps }: AppProps) {
         <WalletProvider>
           <IPFSProvider>
             <SelfIDProvider>
-              <Component {...pageProps} />;
+              <Loader>
+                <Component {...pageProps} />
+              </Loader>
             </SelfIDProvider>
           </IPFSProvider>
         </WalletProvider>
       </FirebaseProvider>
     </QueryClientProvider>
   );
+}
+
+export function Loader({ children }: { children: JSX.Element | null }) {
+  const { selfID } = useSelfID();
+  const { ipfs } = useIpfs();
+  const { address } = useWallet();
+
+  if (!selfID || !ipfs || !address) {
+    return <div>...LOADING...</div>;
+  }
+
+  return children;
 }
 
 export default MyApp;
