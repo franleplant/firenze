a57571981a8f4f193daa82abbb0a5980d9fb96a8
IgnacioAmigo <amigoigna@gmail.com>
2021-11-27 12:06:41 -0300

    Cleanup on management scripts (#64)

---
+++ ceramic-management/create-update-model.ts
@@ -0,0 +1,75 @@
+import { writeFile } from 'node:fs/promises'
+import { CeramicClient } from '@ceramicnetwork/http-client'
+import { ModelManager } from '@glazed/devtools'
+import { DID } from 'dids'
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import { getResolver } from 'key-did-resolver'
+import { fromString } from 'uint8arrays'
+import dotenv from 'dotenv'
+import invariant from "ts-invariant"
+
+import profileModel from "./schemas/profile.json"
+import conversationsModel from "./schemas/conversations.json"
+
+async function main() {
+  // hacky way to work around the fact that calling this from the parent project causes trouble with resolving .env paths
+  // todo: is there a better way to do this? probably 
+  dotenv.config( { path: __dirname + "/.env" });
+
+  // The key must be provided as an environment variable
+  const didKey = process.env.DID_KEY;
+  invariant(didKey);
+  const key = fromString(didKey, 'base16');
+  
+  // Create and authenticate the DID
+  const did = new DID({
+    provider: new Ed25519Provider(key),
+    resolver: getResolver(),
+  });
+  await did.authenticate();
+  
+  console.log(`Authenticated DID ${did.id}`);
+  
+  // Connect to the local Ceramic node
+  const ceramicNodeURL = process.env.CERAMIC_NODE || 'http://localhost:7007';
+  console.log(`Connecting to Ceramic Node on: ${ceramicNodeURL}`);
+  const ceramic = new CeramicClient(ceramicNodeURL);
+  ceramic.did = did;
+  
+  // Create a manager for the model
+  const manager = new ModelManager(ceramic);
+  
+  const profileSchemaSID = await manager.createSchema('Profile', profileModel);
+  const conversationsSchemaSID = await manager.createSchema('Conversations', conversationsModel);
+  
+  const profileSchemaURL = manager.getSchemaURL(profileSchemaSID);
+  const conversationSchemaURL = manager.getSchemaURL(conversationsSchemaSID);
+  
+  invariant(profileSchemaURL);
+  invariant(conversationSchemaURL);
+  
+  await manager.createDefinition('myProfile', {
+    name: 'User Profile',
+    description: 'My Profile at Firenze',
+    schema: profileSchemaURL,
+  });
+
+  await manager.createDefinition('myConversations', {
+    name: 'User Conversations',
+    description: 'My conversations at Firenze',
+    schema: conversationSchemaURL,
+  });
+  
+  // Publish model to Ceramic node
+  const model = await manager.toPublished();
+  
+  // Write model to JSON file
+  await writeFile('./published-model.json', JSON.stringify(model));
+}
+
+main().then(text => {
+  console.log("Finished running scenario");
+})
+.catch(err => {
+  throw err;
+});
