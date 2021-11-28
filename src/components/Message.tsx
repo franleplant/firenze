d6ef6de6eaef78d40084b6481fb2b153e1e8b881
Fran Guijarro <franleplant@gmail.com>
2021-11-28 14:27:00 -0300

    better message ui

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,8 +1,17 @@
 import { useEffect, useRef } from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
+import CircularProgress from "@mui/material/CircularProgress";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { useTheme } from "@mui/material/styles";
+import moment from "moment";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
+import DoneIcon from "@mui/icons-material/Done";
+import HourglassEmptyIcon from "@mui/icons-material/HourglassEmpty";
+import CloudIcon from "@mui/icons-material/Cloud";
+import Link from "@mui/material/Link";
+import Tooltip from "@mui/material/Tooltip";
 
 import { getIpfsPath, IMessage, MsgURL } from "dal/message";
 
@@ -21,6 +30,8 @@ export interface IProps {
 export default function Message(props: IProps) {
   const isFromLocalUser = props.msg?.from === props.address;
 
+  const theme = useTheme();
+
   const onMount = useRef(props.onMount);
   useEffect(() => {
     onMount?.current?.();
@@ -34,41 +45,63 @@ export default function Message(props: IProps) {
   }
 
   let border: any = {
-    borderBottomLeftRadius: "5px",
+    borderBottomLeftRadius: "2px",
   };
   if (isFromLocalUser) {
     border = {
-      borderBottomRightRadius: "5px",
+      borderBottomRightRadius: "2px",
     };
+  }
+
+  const time = moment(props.timestamp).format("HH:mm YYYY-MM-DD");
+
+  let statusIcon = <DoneAllIcon color="success" />;
+  if (props.status === "sending") {
+    statusIcon = <HourglassEmptyIcon />;
+  }
+  if (props.status === "archiving") {
+    statusIcon = <DoneIcon color="info" />;
   }
 
   return (
     <Card
-      elevation={24}
+      elevation={2}
       sx={{
         maxWidth: "500px",
-        // TODO use theme
-        background: isFromLocalUser ? "#3b3232" : "#454545",
+        background: isFromLocalUser
+          ? theme.palette.grey[900]
+          : theme.palette.grey[800],
         alignSelf: isFromLocalUser ? "flex-end" : "flex-start",
         borderRadius: "20px",
         ...border,
       }}
     >
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          {props.timestamp}
-          {` ${props.status}`}
-        </Typography>
-        {link && (
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            <a href={link} target="__blank">
-              ipfs: ...{link.slice(-10)}
-            </a>
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography sx={{ fontSize: 14 }} color={theme.palette.grey[400]}>
+            {time}{" "}
           </Typography>
-        )}
 
-        <Typography variant="body2">
-          {props.isLoading ? "Loading..." : props.msg?.content}
+          {link && (
+            <Tooltip title={link}>
+              <Link href={link} target="__blank" rel="noopener" sx={{lineHeight: "1px"}}>
+                <CloudIcon titleAccess={`open ipfs link`} color="action"/>
+              </Link>
+            </Tooltip>
+          )}
+
+          <Tooltip title={props.status}>
+          {statusIcon}
+          </Tooltip>
+        </Stack>
+
+        <Typography variant="body1" color="inherit" sx={{ marginTop: "10px" }}>
+          {props.isLoading ? <CircularProgress /> : props.msg?.content}
         </Typography>
       </CardContent>
     </Card>
